7cf3f1538f06055b94f3a59652f93112803a7a5d
unknown <olamygol14@gmail.com>
2023-07-27 13:11:50 +0300

    build

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,22 @@
-  
+const builds = document.querySelectorAll('.build');
+const buildValue = document.querySelector('#build-value');
+const stagesValue = document.querySelector('#stages-value');
+const flatsValue = document.querySelector('#flats-value');
+const buildDescriptions = document.querySelector('.build-info__descriptions');
+const buildLinks = document.querySelectorAll('.build-link') 
+ 
+builds.forEach(build => {
+    build.addEventListener('mouseover', function() {
+        const dataBuildNumber = build.getAttribute('data-build-number')
+        const dataStagesNumber = build.getAttribute('data-stages-number')
+        const dataFlatsNumber =  build.getAttribute('data-flats-number')
+        const dataDescriptions =  build.getAttribute('data-descriptions')
+        
+        
+        buildValue.innerHTML = dataBuildNumber
+        stagesValue.innerHTML = dataStagesNumber
+        flatsValue.innerHTML = dataFlatsNumber
+        buildDescriptions.innerHTML = dataDescriptions
+ 
+    })
+})
